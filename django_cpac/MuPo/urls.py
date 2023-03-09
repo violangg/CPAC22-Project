7d47fa5db05bef1cbeaa92e6452b4c6e476007fe
Viola Negroni <violanegroni@yahoo.it>
2023-03-09 21:05:27 +0100

    cose

--- django_cpac/MuPo/urls.py
+++ django_cpac/MuPo/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import index, spotify_auth, spotify_callback, exchange_code, postauth
+from .views import index, spotify_auth, spotify_callback, postauth
 
 urlpatterns = [
     path('', index, name='index'),
     path('spotify_auth', spotify_auth, name='spotify_auth'),
     path('spotify_callback', spotify_callback, name='spotify_callback'),
-    path('exchange_code', exchange_code, name='exchange_code'),
     path('postauth', postauth, name='postauth'),  
 ]
 
@@ -16,6 +15,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('spotify_auth', views.spotify_auth, name='spotify_auth'),
     path('spotify_callback', views.spotify_callback, name='spotify_callback'),
-    path('exchange_code', views.exchange_code, name='exchange_code'),
     path('postauth/', views.postauth, name='postauth'),
 ]
